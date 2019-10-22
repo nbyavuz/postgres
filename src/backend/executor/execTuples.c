43cb873bbc000b465ba673aba4c3fc6c792bc9e5
Andres Freund <andres@anarazel.de>
2019-10-22 13:22:58 -0700

    WIP: jit: add a few additional noinline markers
    Author:
    Reviewed-By:
    Discussion: https://postgr.es/m/
    Backpatch:

--- src/backend/executor/execTuples.c
+++ src/backend/executor/execTuples.c
@@ -1836,7 +1836,7 @@ ExecInitNullTupleSlot(EState *estate, TupleDesc tupType,
  * deforming. That exception aside, there should be no callers outside of this
  * file.
  */
-void
+void pg_noinline
 slot_getmissingattrs(TupleTableSlot *slot, int startAttNum, int lastAttNum)
 {
 	AttrMissing *attrmiss = NULL;
