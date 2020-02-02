18909e8da37995ab58b5f7dcfcb4ca39af621b14
Thomas Munro <thomas.munro@gmail.com>
2020-02-02 22:24:22 +1300

    Add pg_prefetch_mem() macro to load cache lines.
    Initially mapping to GCC, Clang and MSVC builtins.
    
    Discussion: https://postgr.es/m/CAEepm%3D2y9HM9QP%2BHhRZdQ3pU6FShSMyu%3DV1uHXhQ5gG-dketHg%40mail.gmail.com

--- src/include/c.h
+++ src/include/c.h
@@ -360,6 +360,14 @@ typedef void (*pg_funcptr_t) (void);
 #endif
 #endif
 
+/* Do we have support for prefetching memory? */
+#if defined(HAVE__BUILTIN_PREFETCH)
+#define pg_prefetch_mem(a) __builtin_prefetch(a)
+#elif defined(_MSC_VER)
+#define pg_prefetch_mem(a) _m_prefetch(a)
+#else
+#define pg_prefetch_mem(a)
+#endif
 
 /* ----------------------------------------------------------------
  *				Section 2:	bool, true, false
