17d586019dc78d747f1552f091519802df09ba46
Andres Freund <andres@anarazel.de>
2020-05-25 03:02:56 -0700

    xlog: Avoid repeated scans of all xlog insert locks when replacing contents.
    Author:
    Reviewed-By:
    Discussion: https://postgr.es/m/
    Backpatch:

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -583,6 +583,8 @@ typedef struct XLogCtlInsert
 	int			nonExclusiveBackups;
 	XLogRecPtr	lastBackupStart;
 
+	pg_atomic_uint64 knownCompletedUpto;
+
 	/*
 	 * WAL insertion locks.
 	 */
@@ -1777,6 +1779,22 @@ WALInsertLockUpdateInsertingAt(XLogRecPtr insertingAt)
 						insertingAt);
 }
 
+static bool
+XLogInsertionsKnownFinished(XLogRecPtr upto)
+{
+	XLogRecPtr	knownFinishedUpto;
+
+	knownFinishedUpto = (XLogRecPtr) pg_atomic_read_u64(&XLogCtl->Insert.knownCompletedUpto);
+
+	if (upto <= knownFinishedUpto)
+	{
+		pg_read_barrier();
+		return true;
+	}
+	else
+		return false;
+}
+
 /*
  * Wait for any WAL insertions < upto to finish.
  *
@@ -1872,6 +1890,20 @@ WaitXLogInsertionsToFinish(XLogRecPtr upto)
 		if (insertingat != InvalidXLogRecPtr && insertingat < finishedUpto)
 			finishedUpto = insertingat;
 	}
+
+	{
+		XLogRecPtr knownCompletedUpto =
+			(XLogRecPtr) pg_atomic_read_u64(&XLogCtl->Insert.knownCompletedUpto);
+
+		while (knownCompletedUpto < finishedUpto)
+		{
+			if (pg_atomic_compare_exchange_u64(&XLogCtl->Insert.knownCompletedUpto,
+											   &knownCompletedUpto,
+											   finishedUpto))
+				break;
+		}
+	}
+
 	return finishedUpto;
 }
 
@@ -2187,7 +2219,11 @@ AdvanceXLInsertBuffer(XLogRecPtr upto, bool opportunistic)
 				 */
 				LWLockRelease(WALBufMappingLock);
 
-				WaitXLogInsertionsToFinish(OldPageRqstPtr);
+				if (!XLogInsertionsKnownFinished(OldPageRqstPtr))
+				{
+					WaitXLogInsertionsToFinish(OldPageRqstPtr);
+					Assert(XLogInsertionsKnownFinished(OldPageRqstPtr));
+				}
 
 				LWLockAcquire(WALWriteLock, LW_EXCLUSIVE);
 
@@ -2434,6 +2470,8 @@ XLogWrite(XLogwrtRqst WriteRqst, bool flexible)
 
 	/* We should always be inside a critical section here */
 	Assert(CritSectionCount > 0);
+
+	Assert(XLogInsertionsKnownFinished(WriteRqst.Write));
 
 	/*
 	 * Update local LogwrtResult (caller probably did this already, but...)
@@ -5210,6 +5248,8 @@ XLOGShmemInit(void)
 	SpinLockInit(&XLogCtl->info_lck);
 	SpinLockInit(&XLogCtl->ulsn_lck);
 	InitSharedLatch(&XLogCtl->recoveryWakeupLatch);
+
+	pg_atomic_init_u64(&XLogCtl->Insert.knownCompletedUpto, 0);
 }
 
 /*
