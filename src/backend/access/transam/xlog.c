5bea38bca9bfec7ce715f507ee2e850b63898cb3
Andres Freund <andres@anarazel.de>
2021-02-14 13:13:32 -0800

    io: Add o_direct mode.
    Note that this does not yet use AIO, so it'll be very slow.
    
    Todo:
    - figure out precise interplay between fsync=off and io_wal_[init_]direct
    - move osx O_DIRECT handling from md.c to fd.c
    - walreceiver is handled weirdly

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -3272,6 +3272,7 @@ XLogFileInit(XLogSegNo logsegno, bool *use_existent, bool use_lock)
 	XLogSegNo	max_segno;
 	int			fd;
 	int			save_errno;
+	int			open_flags = O_RDWR | O_CREAT | O_EXCL | PG_BINARY;
 
 	XLogFilePath(path, ThisTimeLineID, logsegno, wal_segment_size);
 
@@ -3304,8 +3305,11 @@ XLogFileInit(XLogSegNo logsegno, bool *use_existent, bool use_lock)
 
 	unlink(tmppath);
 
+	if (io_wal_init_direct)
+		open_flags |= PG_O_DIRECT;
+
 	/* do not use get_sync_bit() here --- want to fsync only at end of fill */
-	fd = BasicOpenFile(tmppath, O_RDWR | O_CREAT | O_EXCL | PG_BINARY);
+	fd = BasicOpenFile(tmppath, open_flags);
 	if (fd < 0)
 		ereport(ERROR,
 				(errcode_for_file_access(),
@@ -3888,7 +3892,7 @@ XLogFileClose(void)
 	 * use the cache to read the WAL segment.
 	 */
 #if defined(USE_POSIX_FADVISE) && defined(POSIX_FADV_DONTNEED)
-	if (!XLogIsNeeded())
+	if (!XLogIsNeeded() && !io_wal_direct)
 		(void) posix_fadvise(openLogFile, 0, 0, POSIX_FADV_DONTNEED);
 #endif
 
@@ -10448,10 +10452,17 @@ get_sync_bit(int method)
 {
 	int			o_direct_flag = 0;
 
+	/* make O_DIRECT setting only depend on GUC */
+	if (io_wal_direct)
+		o_direct_flag |= PG_O_DIRECT;
+
+#if 0
 	/* If fsync is disabled, never open in sync mode */
 	if (!enableFsync)
 		return 0;
+#endif
 
+#if 0
 	/*
 	 * Optimize writes by bypassing kernel cache with O_DIRECT when using
 	 * O_SYNC/O_FSYNC and O_DSYNC.  But only if archiving and streaming are
@@ -10460,14 +10471,19 @@ get_sync_bit(int method)
 	 * read if we bypassed the kernel cache. We also skip the
 	 * posix_fadvise(POSIX_FADV_DONTNEED) call in XLogFileClose() for the same
 	 * reason.
-	 *
+	 */
+	if (!XLogIsNeeded())
+		o_direct_flag = PG_O_DIRECT;
+#endif
+
+	/*
 	 * Never use O_DIRECT in walreceiver process for similar reasons; the WAL
 	 * written by walreceiver is normally read by the startup process soon
 	 * after it's written. Also, walreceiver performs unaligned writes, which
 	 * don't work with O_DIRECT, so it is required for correctness too.
 	 */
-	if (!XLogIsNeeded() && !AmWalReceiverProcess())
-		o_direct_flag = PG_O_DIRECT;
+	if (AmWalReceiverProcess())
+		return 0;
 
 	switch (method)
 	{
@@ -10480,7 +10496,7 @@ get_sync_bit(int method)
 		case SYNC_METHOD_FSYNC:
 		case SYNC_METHOD_FSYNC_WRITETHROUGH:
 		case SYNC_METHOD_FDATASYNC:
-			return 0;
+			return o_direct_flag;
 #ifdef OPEN_SYNC_FLAG
 		case SYNC_METHOD_OPEN:
 			return OPEN_SYNC_FLAG | o_direct_flag;
