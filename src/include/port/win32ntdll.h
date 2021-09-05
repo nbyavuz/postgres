abb3cc3159c27f968315942cfdcecdce8932982b
Thomas Munro <thomas.munro@gmail.com>
2021-09-05 23:49:23 +1200

    TMP: DONTMERGE: Check for STATUS_DELETE_PENDING on Windows.
    1.  Update our open() wrapper to check for NT's STATUS_DELETE_PENDING
    and translate it to appropriate errors.
    
    2.  Remove non-working code that was trying to do something similar for
    stat(), and just reuse the open() code.
    
    XXX TODO: resolve order-of-operations problem: pgwin32_open_handle() is
    caled by _pgstat64() before the Windows signal emulation is started up,
    but it might call pg_usleep().
    
    Discussion: https://postgr.es/m/CA%2BhUKGJz_pZTF9mckn6XgSv69%2BjGwdgLkxZ6b3NWGLBCVjqUZA%40mail.gmail.com

---
+++ src/include/port/win32ntdll.h
@@ -0,0 +1,22 @@
+/*-------------------------------------------------------------------------
+ *
+ * win32_ntdll.h
+ *	  Dynamically loaded Windows NT functions.
+ *
+ * Portions Copyright (c) 2021, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ * src/include/port/win32ntdll.h
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "c.h"
+
+#include <ntstatus.h>
+
+typedef NTSTATUS (__stdcall *RtlGetLastNtStatus_t)(void);
+
+extern RtlGetLastNtStatus_t pg_RtlGetLastNtStatus;
+
+extern int initialize_ntdll(void);
