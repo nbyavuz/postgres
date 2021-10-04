ee601c7827027f8428442a62b9d76f83a53fad07
Andres Freund <andres@anarazel.de>
2021-10-04 14:58:54 -0700

    windows: perl: Run tests on a few different perl versions.
    ci-os-only: windows

--- ci/windows_build_config.pl
+++ ci/windows_build_config.pl
@@ -6,5 +6,6 @@ our $config;
 $config->{"tap_tests"} = 1;
 $config->{"asserts"} = 1;
 $config->{"openssl"} = "c:/openssl/1.1.1l/";
+$config->{"perl"} = "c:/perl/strawberry-$ENV{PERL_VERSION}-64bit/perl";
 
 1;
