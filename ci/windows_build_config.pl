1266709fa113056491a5ff06ec55accfb38b7004
Andres Freund <andres@anarazel.de>
2021-10-08 18:05:27 -0700

    ci: windows: run python & perl.
    ci-os-only: windows

--- ci/windows_build_config.pl
+++ ci/windows_build_config.pl
@@ -5,6 +5,10 @@ our $config;
 
 $config->{"tap_tests"} = 1;
 $config->{"asserts"} = 1;
+
+# Optional dependencies
 $config->{"openssl"} = "c:/openssl/1.1.1l/";
+$config->{"perl"} = "c:/strawberry/perl";
+$config->{"python"} = "c:/python/";
 
 1;
