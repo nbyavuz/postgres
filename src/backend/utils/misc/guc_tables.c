046cfc76c772c9500c7e575d1e2042c4017b535e
Andres Freund <andres@anarazel.de>
2022-10-03 21:45:44 -0700

    aio: WIP: mark fsync/wal_sync_method PGC_POSTMASTER.
    The AIO WAL logging code currently isn't safe against changes of either of
    these variables while running. That could be ignored for now, but parallel
    workers end up changing GUCs early during startup.

--- src/backend/utils/misc/guc_tables.c
+++ src/backend/utils/misc/guc_tables.c
@@ -1107,7 +1107,7 @@ struct config_bool ConfigureNamesBool[] =
 		NULL, NULL, NULL
 	},
 	{
-		{"fsync", PGC_SIGHUP, WAL_SETTINGS,
+		{"fsync", PGC_POSTMASTER, WAL_SETTINGS,
 			gettext_noop("Forces synchronization of updates to disk."),
 			gettext_noop("The server will use the fsync() system call in several places to make "
 						 "sure that updates are physically written to disk. This ensures "
@@ -4841,7 +4841,7 @@ struct config_enum ConfigureNamesEnum[] =
 	},
 
 	{
-		{"wal_sync_method", PGC_SIGHUP, WAL_SETTINGS,
+		{"wal_sync_method", PGC_POSTMASTER, WAL_SETTINGS,
 			gettext_noop("Selects the method used for forcing WAL updates to disk."),
 			NULL
 		},
