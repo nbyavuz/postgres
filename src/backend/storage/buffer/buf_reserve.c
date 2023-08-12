84f791626991b2a3ede81e8ea704263011b4c5e7
Andres Freund <andres@anarazel.de>
2023-08-11 18:15:42 -0700

    wip: partially work around self-deadlock danger due to writeahead.

--- src/backend/storage/buffer/buf_reserve.c
+++ src/backend/storage/buffer/buf_reserve.c
@@ -163,6 +163,7 @@ static void
 BufReserveFillClean(BufferAccessStrategy strategy, IOContext io_context)
 {
 	int			refill_count;
+	bool		staged_buffer_writes = false;
 
 	refill_count = bbr.clean_target_size - (bbr.clean_clean_size + bbr.clean_writeback_size);
 
@@ -208,6 +209,7 @@ BufReserveFillClean(BufferAccessStrategy strategy, IOContext io_context)
 			clean->in_walflush = false;
 			clean->in_writeback = true;
 			bbr.clean_writeback_size++;
+			staged_buffer_writes = true;
 			dlist_push_tail(&bbr.clean_writeback_list, &clean->node);
 		}
 		else
@@ -227,7 +229,14 @@ BufReserveFillClean(BufferAccessStrategy strategy, IOContext io_context)
 		ReleaseBuffer(clean->buffer);
 	}
 
-	pgaio_limit_pending(false, bbr.clean_batch_size / 2);
+	/*
+	 * If we staged a buffer write we need to submit that write, the buffer is
+	 * locked for that IO.
+	 */
+	if (staged_buffer_writes)
+		pgaio_submit_pending(false);
+	else
+		pgaio_limit_pending(false, bbr.clean_batch_size / 2);
 }
 
 static void
